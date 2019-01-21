cc55029f886002729e3982dd7b5f7e97404d6a8d
Khinenw <deu07115@gmail.com>
2019-01-21 17:29:35 +0900

    Changed userfile save location, obscured default store

--- src/utils/UserStore.js
+++ src/utils/UserStore.js
@@ -1,3 +1,4 @@
+const {app} = require('electron');
 const crypto = require('crypto');
 const {exists} = require('./utils');
 const fs = require('fs');
@@ -32,12 +33,16 @@ class UserStore {
 		this.derivedHash = null;
 		this.storeInitiated = false;
 		this.state = {};
+
+		// These targets are tend to be reset easily.
 		this.encryptTarget = ['id', 'password'];
 
 		this.launcher = launcher;
 		this.lastSaveRequest = Date.now();
 
-		this.configPath = path.resolve('./data/config.json');
+		this.configPath = path.resolve(app.getPath('userData'), 'config.json');
+
+		this.obfKey = Buffer.from('4EA817B59CA295524EEAAC7EFCA53718BB258D2627FDFB9A76AD3E7830A7BC71', 'hex');
 	}
 
 	async init() {
@@ -75,18 +80,24 @@ class UserStore {
 			return {};
 
 		const {encStore, store} = JSON.parse(await promisify(fs.readFile)(this.configPath, 'utf8'));
+
 		const {encrypted, iv} = encStore;
 		const decipher = crypto.createDecipheriv('aes-256-cbc', this.derivedHash, Buffer.from(iv, 'hex'));
-
 		let decrypted = decipher.update(encrypted, 'base64', 'utf8');
 		decrypted += decipher.final('utf8');
 
-		return Object.assign({}, JSON.parse(decrypted), store);
+		const {encrypted: storeEncrypted} = store;
+		const storeDecipher = crypto.createDecipheriv('aes-256-cbc', this.obfKey, Buffer.alloc(16));
+		let storeDecrypted = storeDecipher.update(storeEncrypted, 'base64', 'utf8');
+		storeDecrypted += storeDecipher.final('utf8');
+
+		return Object.assign({}, JSON.parse(decrypted), JSON.parse(storeDecrypted));
 	}
 
 	async save() {
 		this.lastSaveRequest = Date.now();
 
+		// Separating state to encStore and store
 		const encStore = {};
 		const store = {};
 
@@ -99,22 +110,27 @@ class UserStore {
 			store[key] = this.state[key];
 		});
 
-		const decrypted = JSON.stringify(encStore);
+		// Encrypting encStore in order to store privacy-related information.
 		const iv = await promisify(crypto.randomBytes)(16);
 		const cipher = crypto.createCipheriv('aes-256-cbc', this.derivedHash, iv);
-
-		let encrypted = cipher.update(decrypted, 'utf8', 'base64');
+		let encrypted = cipher.update(JSON.stringify(encStore), 'utf8', 'base64');
 		encrypted += cipher.final('base64');
 
-		const ivText = iv.toString('hex');
+		// Encrypting store in order to make file obscure and hard to edit
+		const storeCipher = crypto.createCipheriv('aes-256-cbc', this.obfKey, Buffer.alloc(16));
+		let storeEncrypted = storeCipher.update(JSON.stringify(store), 'utf8', 'base64');
+		storeEncrypted += storeCipher.final('base64');
 
 		await promisify(fs.writeFile)(this.configPath, JSON.stringify({
 			warning: "주의! 이 파일을 다른 사람에게 공유하지 마세요. 비밀번호 등이 포함된 파일입니다.",
 			encStore: {
-				encrypted, iv: ivText
+				encrypted,
+				iv: iv.toString('hex')
 			},
 
-			store
+			store: {
+				encrypted: storeEncrypted
+			}
 		}, null, '\t'));
 	}
 
